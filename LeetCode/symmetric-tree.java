38d867c9811fa71d0b2667b8b838857d9c259060
TheCoinTosser <tiago.catelam@gmail.com>
2025-09-04 19:57:06 +1000

    LeetCode - https://leetcode.com/problems/symmetric-tree/

---
+++ LeetCode/symmetric-tree.java
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/symmetric-tree/
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isSameTree(root.left, root.right);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q != null) return false;
+        if (p != null && q == null) return false;
+        if (p == null && q == null) return true;
+        return p.val == q.val && isSameTree(p.left, q.right) && isSameTree(p.right, q.left);
+    }
+}
